ee992f0ce1f66d01211d486ab1a2c86cef937cf7
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-06-15 21:09:54 -0300

    adds js, number audios, and problem 1 audio

---
+++ math-game/numbersounds.js
@@ -0,0 +1,29 @@
+const audio01 = new Audio();
+audio01.src = "./tts/tts-01.mp3"
+
+const audio02 = new Audio();
+audio02.src = "./tts/tts-02.mp3"
+
+const audio03 = new Audio();
+audio03.src = "./tts/tts-03.mp3"
+
+const audio04 = new Audio();
+audio04.src = "./tts/tts-04.mp3"
+
+const audio05 = new Audio();
+audio05.src = "./tts/tts-05.mp3"
+
+const audio06 = new Audio();
+audio06.src = "./tts/tts-06.mp3"
+
+const audio07 = new Audio();
+audio07.src = "./tts/tts-07.mp3"
+
+const audio08 = new Audio();
+audio08.src = "./tts/tts-08.mp3"
+
+const audio09 = new Audio();
+audio09.src = "./tts/tts-09.mp3"
+
+const audio10 = new Audio();
+audio10.src = "./tts/tts-10.mp3"
